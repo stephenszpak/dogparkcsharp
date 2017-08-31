21bab9ec13f4ce9715f4c316446d9b4d31b1efa5
Stephen Szpak <srszpak@gmail.com>
2017-08-31 15:05:39 -0500

    initial commit

---
+++ DogParkFinder/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DogParkFinder.Startup))]
+namespace DogParkFinder
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
